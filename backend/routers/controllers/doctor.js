099d0a085bef269a7fba792864cde731d4a387b9
MohammadShoman <mohammad.shoman25@gmail.com>
2021-06-24 11:10:27 +0300

    deleted createDoctor

--- backend/routers/controllers/doctor.js
+++ backend/routers/controllers/doctor.js
@@ -1,15 +1,6 @@
 const db = require("./../../db/db");
 
-const cerateDoctor = (req, res) => {
-    const {firstName,lastName,age,email,password,role_id,img} = req.body;
-    const query=`INSERT INTO users (firstName,lastName,age,email,password,role_id,img) VALUES (?,?,?,?,?,?,?)`
-    const array=[firstName,lastName,age,email,password,role_id,img]
-    db.query(query,array,(err,result)=>{
-        if(err) res.status(404).send(err)
-        res.status(201).json(result)
-    })
-  };
-
+// this function to get all doctors 
 const getAllDoctors = (req, res) => {
   const query = `SELECT * FROM users WHERE role_id=2`;
   db.query(query, (err, result) => {
@@ -18,7 +9,9 @@ const getAllDoctors = (req, res) => {
   });
 };
 
+
+
 module.exports = {
   getAllDoctors,
-  cerateDoctor,
+  
 };
--- backend/routers/routes/doctor.js
+++ backend/routers/routes/doctor.js
@@ -1,9 +1,9 @@
 const express = require("express");
 
-const {getAllDoctors,cerateDoctor}=require("./../controllers/doctor");
+const {getAllDoctors}=require("./../controllers/doctor");
 const doctorRouter = express.Router();
 
 doctorRouter.get("/doctor",getAllDoctors);
-doctorRouter.post("/doctor",cerateDoctor)
+
 
 module.exports = doctorRouter;
