c70917b5f7577aaba0ec288d378d14706d190882
Ghaidaa_tabikh <tabikh.ghaidaa@gmail.com>
2021-06-28 01:53:44 +0300

    navigation bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import Navigation from './components/navigation/index';
 
 const App = () => {
+	<Navigation  />
 	return <div className="App">App component</div>;
+	
 };
 
 export default App;
