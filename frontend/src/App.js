9e925ed49bf016ce6f30f39f8f3d245d24cf1f02
MohammadShoman <mohammad.shoman25@gmail.com>
2021-06-27 20:15:04 +0300

    work on actions and reducer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
-
 const App = () => {
-	return <div className="App">App component</div>;
+	return <div className="App"></div>;
 };
 
 export default App;
