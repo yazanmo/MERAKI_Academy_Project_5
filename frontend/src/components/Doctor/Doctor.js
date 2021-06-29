c757514a5532bc67233a078d05b2126df76b2e63
yazanmo <yazanmostafa96@gmail.com>
2021-06-29 10:10:31 +0300

    end page of all Doctor

--- frontend/src/components/Doctor/Doctor.js
+++ frontend/src/components/Doctor/Doctor.js
@@ -5,14 +5,19 @@ import axios from 'axios';
 import "./Doctor.css";
 
 
-const Doctor = (props) => {
+const Doctor = () => {
   const [Doctor, setDoctor] = useState()
   const [DoctorName, setRDoctorName] = useState()
+  const [num1, setnum1] = useState(0)
+  const [num2, setnum2] = useState(99999999999999)
+  const [filter, setfilter] = useState()
+  
+
   const history = useHistory();
 
   
   useEffect(() => {
-    axios.get(`http://localhost:5000/doctor`)
+    axios.get(`http://localhost:5000/doctor`,{num1,num2})
       .then((response) => {
         setDoctor(response.data)
       }).catch((err) => {
@@ -20,6 +25,19 @@ const Doctor = (props) => {
       })
   }, [])
 
+
+  const callType_1 = () => {
+    axios.post(`http://localhost:5000/doctors`,{num1,num2})
+        .then((response) => {
+     
+         setfilter (response.data)
+         setDoctor ([])
+      }).catch((err) => {
+        console.log("Error")
+      })
+  }
+  
+  
   const func = (id) => {
     return (history.push(`/doctor/${id}`)
     )
@@ -28,10 +46,32 @@ const Doctor = (props) => {
 
   return (
     <>
+         <div className="filter">
+
+        <input onChange={(e)=>{setnum1(e.target.value); }}/>
+        <input onChange={(e)=>{setnum2(e.target.value);}}/>
+        <button onClick={callType_1}>Filter</button>
+      </div>
+              
+             
+
+      {filter && filter.map((elem, i) => {
+                    return (<div className="childrestaurant" key={i}>
+                        <div className="par">
+              <h2>{elem.firstName} {elem.lastName}</h2>
+              <h2>{elem.price}</h2>
+              <p>{elem.description}</p>
+              <button onClick={() => { func(elem.id) }}>more details</button>
+             
+            </div>
+                    </div>)
+                })}
+
       <div className="parantDoctor">
         {Doctor && Doctor.map((elem, i) => {
           return (<div className="childDoctor" key={i} >
               
+             
             <div className="imag">
               <img src={`${elem.img}`} />
             </div>
@@ -42,7 +82,7 @@ const Doctor = (props) => {
               <button onClick={() => { func(elem.id) }}>more details</button>
              
             </div>
-        
+
           </div>)
         })}
       </div>
