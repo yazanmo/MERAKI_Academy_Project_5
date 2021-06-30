0304458f097785f65632aed0a746f9d416ebbdf5
yazanmo <yazanmostafa96@gmail.com>
2021-06-30 21:05:34 +0300

    create signup as admin

--- backend/routers/routes/dashboard.js
+++ backend/routers/routes/dashboard.js
@@ -2,11 +2,14 @@ const express = require("express");
 const {
   createUserDoctor,
   deleteDoctorById,
+  creatAdmin,
 } = require("./../../routers/controllers/dashboard");
 
 const createUserDoctorRouter = express.Router();
 
 createUserDoctorRouter.post("/doctor", createUserDoctor);
 createUserDoctorRouter.delete("/doctor", deleteDoctorById);
+createUserDoctorRouter.post("/admin", creatAdmin);
+
 
 module.exports = createUserDoctorRouter;
