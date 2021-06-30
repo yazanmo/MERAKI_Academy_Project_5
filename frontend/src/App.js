f83e2a4a590a81010d03cd5113c8b004a7475ba3
yazanmo <yazanmostafa96@gmail.com>
2021-06-30 10:31:40 +0300

    craete page admin to get all doctor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,34 +6,34 @@ import DoctorDetails from "./components/doctordetails/doctordetails";
 import Doctor from "./components/Doctor/Doctor";
 
 
-import Admin from "./components/Admin/admin";
+import Info from "./components/doctorinfo/doctorinfo";
 import Navigation from "./components/navigation";
 import DoctorProfile from "./components/doctor_profile/doctor_profile"
 import SignUp from "./components/signUp/signUp";
 import Home from "./components/Home"
-
+import DoctorAdmin from "./components/admin/admin"
 
 const App = () => {
   return (
     <>
       <div className="App">
 
-		  <Navigation/>
-      <Home/>
+<Navigation/>
+<Home/>
 
-        <Route exact path="/login" render={() => <Login />} />
- 
-        <Route exact path="/doctor/:id" render={() => <DoctorDetails />} />
-    
-        <Route exact path="/admin" render={()=> <Admin />}/>
+  <Route exact path="/login" render={() => <Login />} />
 
-        <Route exact path="/doctor" render={() => <Doctor/>} />
+  <Route exact path="/doctor/:id" render={() => <DoctorDetails />} />
 
-		<Route exact path="/register" render={() => <SignUp />} />
-     <Route exact path="/doctorProfile" render={() =><DoctorProfile/>}/>
+  <Route exact path="/doctorInfo" render={()=> <Info />}/>
 
+  <Route exact path="/doctor" render={() => <Doctor/>} />
 
-		</div>
+  <Route exact path="/register" render={() => <SignUp />} />
+  <Route exact path="/doctorProfile" render={() =><DoctorProfile/>}/>
+  <Route exact path="/doctorAdmin" render={() => <DoctorAdmin/>} />
+
+</div>
 
     </>
   );
--- frontend/src/components/Admin/admin.js
+++ frontend/src/components/Admin/admin.js
@@ -1,125 +1,22 @@
-import React, { useState} from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import axios from 'axios';
 import "./admin.css";
 
 
-const Admin = () => {
-    const [FirstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [age, setAge] = useState(0);
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [description, setdescription] = useState("");
-    const [Qualifications, setQualifications] = useState("");
-    const [practicalExperiences, setpracticalExperiences] = useState("");
-    const [qualificationsFile, setqualificationsFile] = useState("");
-    
-   
-    
-    const info = () => {
-       
-      axios
-        .post("http://localhost:5000/info", {
-          FirstName,
-          lastName,
-          age,
-          email,
-          password,
-          description,
-          Qualifications,
-          practicalExperiences,
-          qualificationsFile
-        })
-        .then((result) => {
-          console.log(result.data);
-        })
-        .catch((err) => {
-          
-        });
-    };
-    return (
-      <>
-        <form >
-        <input
-            type="text"
-            placeholder=" First Name "
-            required
-            onChange={(e) => {
-            setFirstName(e.target.value);
-            }}
-          />
-          <input
-            type="text"
-            placeholder=" Last Name "
-            required
-            onChange={(e) => {
-              setLastName(e.target.value);
-            }}
-          />
-          <input
-            type="number"
-            placeholder=" Age "
-            required
-            onChange={(e) => {
-              setAge(e.target.value);
-            }}
-          />
+const DoctorAdmin = () => {
+  const [Doctor, setDoctor] = useState()
+  const [DoctorName, setRDoctorName] = useState()
   
-          <input
-            type="email"
-            placeholder=" Email "
-            required
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
-          />
-          <input
-            type="password"
-            placeholder=" Password "
-            required
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
-          />
 
-           <input
-            type="text"
-            placeholder=" description "
-            required
-            onChange={(e) => {
-                setdescription(e.target.value);
-            }}
-          />
-           <input
-            type="text"
-            placeholder=" Qualifications "
-            required
-            onChange={(e) => {
-               setQualifications(e.target.value);
-            }}
-          />
-           <input
-            type="text"
-            placeholder=" practicalExperiences "
-            required
-            onChange={(e) => {
-              setpracticalExperiences(e.target.value);
-            }}
-          />
-           <input
-            type="text"
-            placeholder=" qualificationsFile "
-            required
-            onChange={(e) => {
-              setqualificationsFile(e.target.value);
-            }}
-          />
-          <button onClick={info}>ok</button>
-          <din className="info" ><p></p> </din>
-  
-        </form>
-       
-      </>
-    );
-  };
-export default Admin
+
+
+
+  return (
+    <>
+     
+
+    </>
+  );
+}
+export default DoctorAdmin
