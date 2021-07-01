881799563217f460cee4e339353270d6ed6ad531
user name <abdalafo.abubaker.com>
2021-07-01 03:24:12 +0300

    add snack

--- frontend/src/components/profile/snack.js
+++ frontend/src/components/profile/snack.js
@@ -4,6 +4,22 @@ import axios from "axios";
 const Snack = () => {
   const [query, setQuery] = useState("");
   const [result, setResult] = useState([]);
+  const [infoFood, setInfoFood] = useState(false);
+  const [getBreakfast, setGetBreakfast] = useState([]);
+  const token = localStorage.getItem("token");
+  //   console.log("getBreakfast", getBreakfast);
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/breakfast", {
+        headers: {
+          authorization: "Bearer " + token,
+        },
+      })
+      .then((res) => {
+        setGetBreakfast(res.data);
+      })
+      .catch((err) => {});
+  }, [getBreakfast]);
 
   return (
     <div>
@@ -28,6 +44,7 @@ const Snack = () => {
         }}
       >
         <input
+          id="input-breakfast"
           onChange={(e) => {
             console.log(query);
             console.log(result);
@@ -38,25 +55,76 @@ const Snack = () => {
         <button type="submit">ok</button>
         {result &&
           result.map((elem, i) => {
+            let name = elem.name;
             return (
-              <div className="childDoctor" key={i}>
-                <div className="par">
-                  <h2>{elem.name}</h2>
-                  <h2>{elem.calories}</h2>
-                  <h2>{elem.carbohydrates_total_g}</h2>
-                  <h2>{elem.cholesterol_mg}</h2>
-                  <h2>{elem.fat_saturated_g}</h2>
-                  <h2>{elem.fiber_g}</h2>
-                  <h2>{elem.potassium_mg}</h2>
-                  <h2>{elem.protein_g}</h2>
-                  <h2>{elem.serving_size_g}</h2>
-                  <h2>{elem.sodium_mg}</h2>
-                  <h2>{elem.sugar_g}</h2>
-                </div>
+              <div
+                className="desc-food-tracker"
+                key={i}
+                onClick={() => {
+                  axios
+                    .post(
+                      `http://localhost:5000/breakfast`,
+                      { name },
+                      {
+                        headers: {
+                          authorization: "Bearer " + token,
+                        },
+                      }
+                    )
+                    .then((res) => {
+                      setResult([]);
+                    })
+                    .catch((err) => {
+                      console.log(err);
+                    });
+                  document.getElementById("input-breakfast").value = "";
+                }}
+              >
+                <h2>{elem.name}</h2>
+                <h2>{elem.calories}</h2>
+                <h2>{elem.carbohydrates_total_g}</h2>
+                <h2>{elem.cholesterol_mg}</h2>
+                <h2>{elem.fat_saturated_g}</h2>
+                <h2>{elem.fiber_g}</h2>
+                <h2>{elem.potassium_mg}</h2>
+                <h2>{elem.protein_g}</h2>
+                <h2>{elem.serving_size_g}</h2>
+                <h2>{elem.sodium_mg}</h2>
+                <h2>{elem.sugar_g}</h2>
               </div>
             );
           })}
       </form>
+      {getBreakfast.map((element, index) => {
+        const id = element.breakfast_id;
+        // console.log("element", breakfast_id);
+        return (
+          <div
+            id={element.breakfast_id}
+            onClick={() => {
+              axios
+                .delete(
+                  "http://localhost:5000/breakfast",
+                  {
+                    headers: {
+                      authorization: "Bearer " + token,
+                    },
+                  },
+                  { id }
+                )
+                .then((res) => {
+                  console.log(res);
+                })
+                .catch((err) => {});
+            }}
+            key={index + 1}
+            class="desc-food-tracker"
+          >
+            {index + 1} {element.breakfast}
+            <br />
+          </div>
+        );
+      })}
     </div>
   );
 };
