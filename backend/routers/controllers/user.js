ddb4cb4c6646a289cd97fd5bc8b81f48e3b3ae8a
user name <abdalafo.abubaker.com>
2021-07-02 03:39:24 +0300

    add update profile

--- backend/routers/controllers/user.js
+++ backend/routers/controllers/user.js
@@ -1,51 +1,47 @@
 const db = require("./../../db/db");
 
+// this function to get the user information
 
-// this function to get the user information 
-
-const getUserInformation=(req,res)=>{
-const id=req.token.id;
-const query=`SELECT * FROM users WHERE id=${id}`
-db.query(query,(err,result)=>{
-    if(err) res.status(400).send(err)
-    res.status(200).json(result)
-})
-}
-
+const getUserInformation = (req, res) => {
+  const id = req.token.id;
+  const query = `SELECT * FROM users WHERE id=${id}`;
+  db.query(query, (err, result) => {
+    if (err) res.status(400).send(err);
+    res.status(200).json(result);
+  });
+};
 
 //this function for update information for user
 
- const updateUserInformationById=(req,res)=>{
-    const id=req.token.id;
-    const {firstName ,lastName,age ,email,password,role_id,img}=req.body
-    const query=`UPDATE users
-    SET firstName=? ,lastName=?,age=? ,email=?,password=?,role_id=?,img=?
-    WHERE id=${id}`
-    const array=[firstName ,lastName,age ,email,password,role_id,img]
-    db.query(query,array,(err,result)=>{
-        if(err) res.status(400).send(err)
-        res.status(200).json(result)
-    })
- }
+const updateUserInformationById = (req, res) => {
+  const id = req.token.id;
+  const { firstName, lastName, age, email, img } = req.body;
+  const query = `UPDATE users
+    SET firstName=? ,lastName=?,age=? ,email=?,img=?
+    WHERE id=${id}`;
+  const array = [firstName, lastName, age, email, img];
+  db.query(query, array, (err, result) => {
+    if (err) res.status(400).send(err);
+    res.status(200).json(result);
+  });
+};
 
 //this function for delete user by id
 
- const deleteUserById=(req,res)=>{
-     const id =req.token.id;
-     const query=`UPDATE users
+const deleteUserById = (req, res) => {
+  const id = req.token.id;
+  const query = `UPDATE users
      SET is_deleted=1
-     WHERE id=${id}`
-    
-     db.query(query,(err,result)=>{
-         if(err) res.status(400).send(err)
-         res.status(200).json(result)
-     })
- }
+     WHERE id=${id}`;
 
+  db.query(query, (err, result) => {
+    if (err) res.status(400).send(err);
+    res.status(200).json(result);
+  });
+};
 
-
-module.exports={
-    getUserInformation,
-    updateUserInformationById,
-    deleteUserById
-}
+module.exports = {
+  getUserInformation,
+  updateUserInformationById,
+  deleteUserById,
+};
--- frontend/src/components/profile/updateuserinfo.js
+++ frontend/src/components/profile/updateuserinfo.js
@@ -4,47 +4,84 @@ import axios from "axios";
 
 const UpdateUserInfo = () => {
   const userInfo = JSON.parse(localStorage.getItem("user_info"));
+  const token = localStorage.getItem("token");
 
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [age, setAge] = useState("");
-  const [img, setImg] = useState("");
-  const [email, setEmail] = useState("");
+  const [firstName, setFirstName] = useState(userInfo.firstName);
+  const [lastName, setLastName] = useState(userInfo.lastName);
+  const [age, setAge] = useState(userInfo.age);
+  const [img, setImg] = useState(userInfo.img);
+  const [email, setEmail] = useState(userInfo.email);
 
+  const history = useHistory();
+
+  const updateProfile = () => {
+    axios
+      .put(
+        "http://localhost:5000/profile",
+        {
+          firstName,
+          lastName,
+          age,
+          email,
+          img,
+        },
+        {
+          headers: {
+            authorization: "Bearer " + token,
+          },
+        }
+      )
+      .then((result) => {
+        history.push("/profile");
+      })
+      .catch((err) => {});
+  };
   return (
     <div>
-      <form>
-        <input
-          defaultValue={userInfo.email}
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-        />
-        <input
-          defaultValue={userInfo.age}
-          onChange={(e) => {
-            setAge(e.target.value);
-          }}
-        />
-        <input
-          defaultValue={userInfo.firstName}
-          onChange={(e) => {
-            setFirstName(e.target.value);
-          }}
-        />
-        <input
-          defaultValue={userInfo.lastName}
-          onChange={(e) => {
-            setLastName(e.target.value);
-          }}
-        />
-        <input
-          defaultValue={userInfo.img}
-          onChange={(e) => {
-            setImg(e.target.value);
-          }}
-        />
-      </form>
+      <input
+        placeholder={userInfo.email}
+        defaultValue={userInfo.email}
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+        readOnly
+      />
+      <input
+        placeholder={userInfo.age}
+        defaultValue={userInfo.age}
+        onChange={(e) => {
+          setAge(e.target.value);
+        }}
+      />
+      <input
+        placeholder={userInfo.firstName}
+        defaultValue={userInfo.firstName}
+        onChange={(e) => {
+          setFirstName(e.target.value);
+        }}
+      />
+      <input
+        placeholder={userInfo.lastName}
+        defaultValue={userInfo.lastName}
+        onChange={(e) => {
+          setLastName(e.target.value);
+        }}
+      />
+      <input
+        placeholder={userInfo.img}
+        defaultValue={userInfo.img}
+        onChange={(e) => {
+          setImg(e.target.value);
+        }}
+        type="text"
+      />
+      <button
+        onClick={() => {
+          updateProfile();
+        }}
+      >
+        update
+      </button>
     </div>
   );
 };
