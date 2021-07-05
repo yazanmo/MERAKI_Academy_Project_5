a1b45dc17c36b9168ce5ad834cac0e9f6941d95e
user name <abdalafo.abubaker.com>
2021-07-05 21:18:03 +0300

    add getMyPatient function

--- backend/routers/controllers/mypatient.js
+++ backend/routers/controllers/mypatient.js
@@ -17,56 +17,53 @@ const getMyDoctor = (req, res) => {
   const user_id = req.token.id;
   const query = ` SELECT doctorsDetails.user_id AS doctor_id
   FROM purchased
-      LEFT JOIN users
-          ON  users.id =purchased.user_id
-          LEFT JOIN doctorsDetails
-          ON  doctorsDetails.id =purchased.doctorsService_id WHERE users.id = ? `;
+  LEFT JOIN users
+  ON  users.id =purchased.user_id
+  LEFT JOIN doctorsDetails
+  ON  doctorsDetails.id =purchased.doctorsService_id WHERE users.id = ? `;
   const data = [user_id];
   db.query(query, data, (err, result) => {
     if (err) res.status(400).send(err);
-    console.log(result[0].doctor_id);
     const query = `SELECT users.firstName ,users.lastName  ,doctorsDetails.description , doctorsDetails.user_id AS doctor_id,doctorsDetails.price
     FROM doctorsDetails
-        INNER JOIN users
-            ON  users.id = doctorsDetails.user_id WHERE users.id = ? `;
+    INNER JOIN users
+    ON  users.id = doctorsDetails.user_id `;
     const data = [result[0].doctor_id];
 
     db.query(query, data, (err, results) => {
       if (err) res.status(400).send(err);
-      console.log(result);
 
       res.status(200).json(results);
     });
   });
+};
 
-  const getMyPatient= ()=>{
-    const user_id = req.token.id;
-    const query = ` SELECT doctorsDetails.user_id AS doctor_id
+const getMyPatient = (req, res) => {
+  const user_id = req.token.id;
+  const query = ` SELECT doctorsDetails.user_id AS user_id
     FROM purchased
-        LEFT JOIN users
-            ON  users.id =purchased.user_id
-            LEFT JOIN doctorsDetails
-            ON  doctorsDetails.id =purchased.doctorsService_id WHERE users.id = ? `;
-    const data = [user_id];
-    db.query(query, data, (err, result) => {
+    LEFT JOIN users
+    ON  users.id =purchased.doctorsService_id
+    LEFT JOIN doctorsDetails
+     ON  doctorsDetails.id =purchased.doctorsService_id WHERE users.id = ? `;
+  const data = [user_id];
+  db.query(query, data, (err, result) => {
+    if (err) res.status(400).send(err);
+    const query = `SELECT users.firstName ,users.lastName  ,users.age , doctorsDetails.user_id AS user_id
+      FROM doctorsDetails
+      INNER JOIN users
+      ON  users.id = doctorsDetails.user_id `;
+    const data = [result[0].user_id];
+
+    db.query(query, data, (err, results) => {
       if (err) res.status(400).send(err);
-      console.log(result[0].doctor_id);
-
-  }
-  //   `
-  // SELECT
-
-  // ``SELECT users.firstName ,users.lastName , purchased.* ,doctorsDetails.description , doctorsDetails.user_id AS doctor_id,doctorsDetails.price
-  //           FROM purchased
-  //               INNER JOIN users
-  //                   ON  users.id =purchased.user_id
-  //                   INNER JOIN doctorsDetails
-  //                   ON  doctorsDetails.id =purchased.doctorsService_id
-  //                   INNER JOIN doctorsDetails
-  //                   ON  doctorsDetails.users_id =users.id`;
+      res.status(200).json(results);
+    });
+  });
 };
 
 module.exports = {
   buyService,
   getMyDoctor,
+  getMyPatient,
 };
--- backend/routers/routes/mypatient.js
+++ backend/routers/routes/mypatient.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const authentication = require("./../middlewares/authentication");
 
-const { buyService, getMyDoctor } = require("./../controllers/mypatient");
+const {
+  buyService,
+  getMyDoctor,
+  getMyPatient,
+} = require("./../controllers/mypatient");
 
 const myPatient = express.Router();
 
 myPatient.post("/mypatient/:id", authentication, buyService);
 myPatient.get("/mydoctor", authentication, getMyDoctor);
+myPatient.get("/mypatient", authentication, getMyPatient);
 
 module.exports = myPatient;
