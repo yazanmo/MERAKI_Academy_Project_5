54fff6bb413028fd026256efc5bfa75e228ccf98
user name <abdalafo.abubaker.com>
2021-07-05 19:04:19 +0300

    add getMyDoctor function

--- backend/routers/controllers/mypatient.js
+++ backend/routers/controllers/mypatient.js
@@ -13,6 +13,46 @@ const buyService = (req, res) => {
   });
 };
 
+const getMyDoctor = (req, res) => {
+  const user_id = req.token.id;
+  const query = ` SELECT doctorsDetails.user_id AS doctor_id
+  FROM purchased
+      LEFT JOIN users
+          ON  users.id =purchased.user_id
+          LEFT JOIN doctorsDetails
+          ON  doctorsDetails.id =purchased.doctorsService_id WHERE users.id = ? `;
+  const data = [user_id];
+  db.query(query, data, (err, result) => {
+    if (err) res.status(400).send(err);
+    console.log(result[0].doctor_id);
+    const query = `SELECT users.firstName ,users.lastName  ,doctorsDetails.description , doctorsDetails.user_id AS doctor_id,doctorsDetails.price
+    FROM doctorsDetails
+        INNER JOIN users
+            ON  users.id = doctorsDetails.user_id WHERE users.id = ? `;
+    const data = [result[0].doctor_id];
+
+    db.query(query, data, (err, results) => {
+      if (err) res.status(400).send(err);
+      console.log(result);
+
+      res.status(200).json(results);
+    });
+  });
+
+  //   `
+  // SELECT
+
+  // ``SELECT users.firstName ,users.lastName , purchased.* ,doctorsDetails.description , doctorsDetails.user_id AS doctor_id,doctorsDetails.price
+  //           FROM purchased
+  //               INNER JOIN users
+  //                   ON  users.id =purchased.user_id
+  //                   INNER JOIN doctorsDetails
+  //                   ON  doctorsDetails.id =purchased.doctorsService_id
+  //                   INNER JOIN doctorsDetails
+  //                   ON  doctorsDetails.users_id =users.id`;
+};
+
 module.exports = {
   buyService,
+  getMyDoctor,
 };
--- backend/routers/routes/mypatient.js
+++ backend/routers/routes/mypatient.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const authentication = require("./../middlewares/authentication");
 
-const { buyService } = require("./../controllers/mypatient");
+const { buyService, getMyDoctor } = require("./../controllers/mypatient");
 
 const myPatient = express.Router();
 
 myPatient.post("/mypatient/:id", authentication, buyService);
+myPatient.get("/mydoctor", authentication, getMyDoctor);
 
 module.exports = myPatient;
