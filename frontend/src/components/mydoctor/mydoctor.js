4adf6b734e05199f4e9ecac028905a9419c6e7bf
user name <abdalafo.abubaker.com>
2021-07-05 21:53:42 +0300

    getMyDoctor frontend

---
+++ frontend/src/components/mydoctor/mydoctor.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const MyDoctor = () => {
+  let token = localStorage.getItem("token");
+  const [result, setResult] = useState([]);
+  return <div></div>;
+};
+
+export default MyDoctor;
