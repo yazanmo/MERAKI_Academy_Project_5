14a815e52788af81e3034d51f275b1bb3a3ec178
user name <abdalafo.abubaker.com>
2021-07-06 11:05:58 +0300

    add myoctor

--- frontend/src/components/mydoctor/mydoctor.js
+++ frontend/src/components/mydoctor/mydoctor.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 const MyDoctor = () => {
   let token = localStorage.getItem("token");
+
   const [result, setResult] = useState([]);
+  const [sa, setSa] = useState(false);
+
   useEffect(() => {
     axios
       .get("http://localhost:5000/mydoctor", {
@@ -14,6 +18,11 @@ const MyDoctor = () => {
       .then((result) => {
         console.log(result.data);
         setResult(result.data);
+        if (sa) {
+          setSa(false);
+        } else {
+          setSa(true);
+        }
       })
       .catch((err) => {
         console.log(err);
@@ -26,7 +35,6 @@ const MyDoctor = () => {
           <div>
             <p> {element.img}</p>
             <p> {element.firstName}</p>
-            <p> {element.lastName}</p>
             <p> {element.lastName}</p>
             <p> {element.description}</p>
           </div>
