409353afd603d5dbfa43dbc6a244e162e88927b4
user name <abdalafo.abubaker.com>
2021-07-06 23:13:53 +0300

    add com patint

--- frontend/src/components/mypatint/patient.js
+++ frontend/src/components/mypatint/patient.js
@@ -1,6 +1,9 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import { useHistory, useParams } from "react-router-dom";
+import axios from "axios";
 const Patient = () => {
+  const { id } = useParams();
+
   return <div></div>;
 };
 
