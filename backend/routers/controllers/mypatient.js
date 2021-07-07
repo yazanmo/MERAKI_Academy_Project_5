f09b95523545a0c8198cd5e3467e076f48d36509
user name <abdalafo.abubaker.com>
2021-07-07 15:33:25 +0300

    add getMyDoctor fun

--- backend/routers/controllers/mypatient.js
+++ backend/routers/controllers/mypatient.js
@@ -15,37 +15,29 @@ const buyService = (req, res) => {
 
 const getMyDoctor = (req, res) => {
   const user_id = req.token.id;
-  const query = ` SELECT doctorsDetails.user_id AS doctor_id
-  FROM purchased
-  LEFT JOIN users
-  ON  users.id =purchased.user_id
-  LEFT JOIN doctorsDetails
-  ON  doctorsDetails.id =purchased.doctorsService_id WHERE users.id = ? `;
+  const query = `SELECT firstName ,lastName FROM doctorsDetails JOIN purchased ON purchased.doctorsService_id=doctorsDetails.id
+   JOIN users as u on u.id= doctorsDetails.user_id where purchased.user_id= ?;
+  `;
   const data = [user_id];
 
   db.query(query, data, (err, result) => {
     if (err) res.status(400).send(err);
-    const query = `SELECT users.firstName ,users.lastName  ,doctorsDetails.description  ,doctorsDetails.id AS id_service  ,users.img, doctorsDetails.user_id AS doctor_id,doctorsDetails.price
-    FROM doctorsDetails
-    INNER JOIN users
-    ON  users.id = doctorsDetails.user_id `;
-    const data = result[0] ? [result[0].doctor_id] : [];
-
-    db.query(query, data, (err, results) => {
-      if (err) res.status(400).send(err);
-
-      res.status(200).json(results);
-    });
+    res.status(200).json(result);
   });
 };
 
 const getMyPatient = (req, res) => {
   const user_id = req.token.id;
+
+  const query = `SELECT firstName ,lastName FROM doctorsDetails JOIN purchased ON purchased.doctorsService_id=doctorsDetails.id
+   JOIN users as u on u.id= purchased.user_id where purchased.user_id= ?;
+  `;
+
   const query = ` SELECT doctorsDetails.user_id AS user_id
     FROM purchased
-    LEFT JOIN users
+    INNER JOIN users
     ON  users.id =purchased.doctorsService_id
-    LEFT JOIN doctorsDetails
+    INNER JOIN doctorsDetails
      ON  doctorsDetails.id =purchased.doctorsService_id WHERE users.id = ? `;
   const data = [user_id];
   db.query(query, data, (err, result) => {
