95d49d80cb610b6be67d9478451db6eb683e12bf
user name <abdalafo.abubaker.com>
2021-07-07 16:05:19 +0300

    add getMyPatient fun

--- backend/routers/controllers/mypatient.js
+++ backend/routers/controllers/mypatient.js
@@ -28,30 +28,13 @@ const getMyDoctor = (req, res) => {
 
 const getMyPatient = (req, res) => {
   const user_id = req.token.id;
-
-  const query = `SELECT firstName ,lastName FROM doctorsDetails JOIN purchased ON purchased.doctorsService_id=doctorsDetails.id
-   JOIN users as u on u.id= purchased.user_id where purchased.user_id= ?;
+  const query = `SELECT  firstName ,lastName  FROM users JOIN purchased ON purchased.user_id=users.id
+  JOIN doctorsDetails ON purchased.doctorsService_id=doctorsDetails.id WHERE purchased.doctorsService_id AND doctorsDetails.user_id= ? ;
   `;
-
-  const query = ` SELECT doctorsDetails.user_id AS user_id
-    FROM purchased
-    INNER JOIN users
-    ON  users.id =purchased.doctorsService_id
-    INNER JOIN doctorsDetails
-     ON  doctorsDetails.id =purchased.doctorsService_id WHERE users.id = ? `;
   const data = [user_id];
   db.query(query, data, (err, result) => {
     if (err) res.status(400).send(err);
-    const query = `SELECT users.firstName ,users.lastName  ,users.age , users.img,doctorsDetails.user_id AS user_id
-      FROM doctorsDetails
-      INNER JOIN users
-      ON  users.id = doctorsDetails.user_id `;
-    // const data = [result[0].user_id];
-
-    db.query(query, (err, results) => {
-      if (err) res.status(400).send(err);
-      res.status(200).json(results);
-    });
+    res.status(200).json(result);
   });
 };
 
