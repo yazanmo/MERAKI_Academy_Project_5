78166f0236d0689e8c2071ae8577c3e47920caa4
MohammadShoman <mohammad.shoman25@gmail.com>
2021-07-08 20:43:02 +0300

    edit  schedule

--- frontend/src/components/schedule/schedule.js
+++ frontend/src/components/schedule/schedule.js
@@ -6,7 +6,7 @@ const Schedule = () => {
   const token = localStorage.getItem("token");
   const { id } = useParams();
   const [result, setResult] = useState([]);
-  const [btata,setBtata]=useState("")
+  const [btata, setBtata] = useState("");
   const [time, setTime] = useState("");
   const [date, setDate] = useState("");
   const [sa, setSa] = useState(false);
@@ -16,54 +16,72 @@ const Schedule = () => {
       .get(`http://localhost:5000/doctor/${id}`)
       .then((result) => {
         setResult(result.data);
-        console.log("554545454545454541",result.data);
       })
       .catch((err) => {});
   }, []);
 
-
-  const info = (req,res) => {
+  const info = (req, res) => {
     axios
-      .post(`http://localhost:5000/schedule/${id}`, {
-        time,
-        date,
-      })
+      .post(
+        `http://localhost:5000/schedule/${id}`,
+        {
+          time,
+          date,
+        },
+        {
+          headers: {
+            authorization: "Bearer " + token,
+          },
+        }
+      )
       .then((result) => {
         console.log(result.data);
-       
       })
       .catch((err) => {
-          res.status(400).send(err);
+        res.status(400).send(err);
       });
   };
 
   return (
     <div>
-      {result&&result.map((element, index) => {
-        return (
-          <div>
-            <p> {element.img}</p>
-            <p> {element.firstName}</p>
-            <p> {element.lastName}</p>
-            <p> {element.price}</p>
-            <p> {element.description}</p>
-          </div>
-        );
-      })}
+      {result &&
+        result.map((element, index) => {
+          return (
+            <div>
+              <p> {element.img}</p>
+              <p> {element.firstName}</p>
+              <p> {element.lastName}</p>
+              <p> {element.price}</p>
+              <p> {element.description}</p>
+            </div>
+          );
+        })}
       <div>
-                        <label className="timetext" >Choose Time: </label>
-                        <input className="time" type="time" name="time" onChange={(e) => { setTime(e.target.value) }} />
-
-                        </div>
+        <label className="timetext">Choose Time: </label>
+        <input
+          className="time"
+          type="time"
+          name="time"
+          onChange={(e) => {
+            setTime(e.target.value);
+          }}
+        />
+      </div>
 
       <div>
-                        <label className="datetext">Choose Date: </label>
-                        <input className="date" type="date"  min="2021-06-12" max="2021-06-30" onChange={(e) => { setDate(e.target.value) }} />
-                        </div>
-
+        <label className="datetext">Choose Date: </label>
+        <input
+          className="date"
+          type="date"
+          min="2021-06-12"
+          max="2021-06-30"
+          onChange={(e) => {
+            setDate(e.target.value);
+          }}
+        />
+      </div>
+      <button onClick={info}>desdffdd</button>
     </div>
-    
   );
-  
 };
 export default Schedule;
