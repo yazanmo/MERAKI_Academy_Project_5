8cfd02e2aba37e286789faf87b100d694465df7b
abdalafo98 <abdalafo.abubaker@gmail.com>
2021-07-09 00:13:25 +0300

    add breakfast table

--- frontend/src/components/mypatint/patient.js
+++ frontend/src/components/mypatint/patient.js
@@ -3,7 +3,12 @@ import { useParams } from "react-router-dom";
 
 import axios from "axios";
 const Patient = () => {
-  const [result, setResult] = useState([]);
+  const [breakfast, setBreakfast] = useState([]);
+  const [Lunch, setLunch] = useState([]);
+  const [dinner, setDinner] = useState([]);
+  const [snack, setSnack] = useState([]);
+  const [activeTime, setActiveTime] = useState([]);
+  const [glassesOfWater, setGlassesOfWater] = useState([]);
   const { id } = useParams();
   const heading = [
     "Name",
@@ -23,7 +28,12 @@ const Patient = () => {
       .get(`http://localhost:5000/patient/${id}`)
       .then((result) => {
         console.log(result.data);
-        setResult(result.data.breakfast.result1);
+        setBreakfast(result.data.breakfast.result1);
+        setLunch(result.data.lunch.result3);
+        setDinner(result.data.dinner.result4);
+        setSnack(result.data.snack.result2);
+        setActiveTime(result.data.activeTime.result6);
+        setGlassesOfWater(result.data.glassesOfWater.result5);
       })
       .catch((err) => {
         console.log(err);
@@ -34,50 +44,35 @@ const Patient = () => {
     <div className="food">
       <table style={{ width: 500 }}>
         <thead>
+          <h1 className="center">breakfast</h1>
+
           <tr>
             {heading.map((head) => (
               <th>{head}</th>
             ))}
           </tr>
         </thead>
-        <h1 className="center">breakfast</h1>
 
         <tbody>
-          {result.map((element) => (
-            <tr>
-              <th>{element.name}</th>
-              <th>{element.calories}</th>
-              <th>{element.serving_size_g}</th>
-              <th>{element.sugar_g}</th>
-              <th>{element.protein_g}</th>
-              <th>{element.carbohydrates_total_g}</th>
-              <th>{element.cholesterol_mg}</th>
-              <th>{element.fat_saturated_g}</th>
-              <th>{element.potassium_mg}</th>
-              <th>{element.sodium_mg}</th>
-            </tr>
-          ))}
-        </tbody>
-        <h1 className="center">breakfast</h1>
-        <tbody>
-          {result.map((element) => (
-            <tr>
-              <th>{element.name}</th>
-              <th>{element.calories}</th>
-              <th>{element.serving_size_g}</th>
-              <th>{element.sugar_g}</th>
-              <th>{element.protein_g}</th>
-              <th>{element.carbohydrates_total_g}</th>
-              <th>{element.cholesterol_mg}</th>
-              <th>{element.fat_saturated_g}</th>
-              <th>{element.potassium_mg}</th>
-              <th>{element.sodium_mg}</th>
-            </tr>
-          ))}
+          {breakfast &&
+            breakfast.map((element) => (
+              <tr>
+                <th>{element.name}</th>
+                <th>{element.calories}</th>
+                <th>{element.serving_size_g}</th>
+                <th>{element.sugar_g}</th>
+                <th>{element.protein_g}</th>
+                <th>{element.carbohydrates_total_g}</th>
+                <th>{element.cholesterol_mg}</th>
+                <th>{element.fat_saturated_g}</th>
+                <th>{element.potassium_mg}</th>
+                <th>{element.sodium_mg}</th>
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
   );
 };
-
+// snack;
 export default Patient;
