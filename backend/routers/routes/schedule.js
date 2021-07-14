cdaafe3c72a4322faee4ab77e22ea1857138643c
MohammadShoman <mohammad.shoman25@gmail.com>
2021-07-14 03:06:46 +0300

    delete schedule for user and doctor

--- backend/routers/routes/schedule.js
+++ backend/routers/routes/schedule.js
@@ -2,12 +2,15 @@ const express = require("express");
 const authentication = require("./../middlewares/authentication");
 
 
-const{bookTime,getBookTime,getMyDoctorById,getScheduleUser}=require("./../controllers/schedule")
+const{bookTime,getBookTime,getMyDoctorById,getScheduleUser,deleteScheduleUser,deleteScheduleDoctor}=require("./../controllers/schedule")
 
 const scheduleRoute = express.Router();
 
 scheduleRoute.post("/schedule/:id", authentication, bookTime);
 scheduleRoute.get("/schedule", authentication, getBookTime);
 scheduleRoute.get("/userSchedule",authentication,getScheduleUser)
+scheduleRoute.delete("/delete-scheduleUser",authentication,deleteScheduleUser)
+scheduleRoute.delete("/delete-scheduleDoctor",deleteScheduleDoctor)
+
 module.exports = scheduleRoute;
 
