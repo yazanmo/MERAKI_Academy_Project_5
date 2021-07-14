b21b096f5772799f278493a284f45f1f1539c1d4
yazanmo <yazanmostafa96@gmail.com>
2021-07-14 13:24:27 +0300

    add style in booking page

--- frontend/src/components/bookingschedule/bookingschedule.js
+++ frontend/src/components/bookingschedule/bookingschedule.js
@@ -26,25 +26,24 @@ const Booking = () => {
   }, []);
 
   return (
-    <div>
+    <div className="book-card">
       {result &&
         result.map((element, index) => {
           return (
-            <div className="book-card">
-  <Card style={{ width: '40rem' }}>
+            <div >
+  <Card style={{ width: "20rem" ,height:"33rem"}}>
     <div className="parent-book">
-    <div className="card-img-book">
-  <Card.Img variant="top"  src={element.img} />
+    <div className="card-img-booka">
+  <Card.Img className="card-img-book"  src={element.img} />
   </div>
   <div className="card-body">
   <Card.Body>
     <Card.Title>{element.firstName} {element.lastName}</Card.Title>
     <Card.Text>
-      <br></br>
+      
               <p>age: {element.age}</p>
               <p>Booking time: {element.time}</p>
               <p>Booking date: {element.date}</p>
-              <br></br>
               <div>
         </div>
     </Card.Text>
