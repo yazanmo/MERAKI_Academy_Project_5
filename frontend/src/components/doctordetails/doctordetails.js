e37960e97764f1f49147b26f783351449263bcbb
abdalafo98 <abdalafo.abubaker@gmail.com>
2021-07-17 02:58:08 +0300

    modify doctors page d

--- frontend/src/components/doctordetails/doctordetails.js
+++ frontend/src/components/doctordetails/doctordetails.js
@@ -157,53 +157,79 @@ const DoctorDetails = ({ setPaymentId, setPaymentReceiver, setPrice }) => {
         rel="stylesheet"
         type="text/css"
       ></link>
-      {/* <div className="name-Q"></div> */}
       <div className="parent">
         <div className="img111">
           <img
-            style={{ width: "500px", height: "400px" }}
+            style={{ width: "350px", height: "300px" }}
             src={result.img ? result.img : <></>}
           />
         </div>
         <div className="decs-do">
-          <p>
-            <span>Dr .</span> {result.firstName} {result.lastName}
+          <p style={{ fontSize: "20px", fontWeight: "bold" }}>
+            <span>Dr</span> {result.firstName.toUpperCase()}{" "}
+            {result.lastName.toUpperCase()}
+          </p>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              margin: "-6% 0px 10% -19%",
+            }}
+          >
             <Stars stars={avgRating} defaultValue={avgRating} />
+          </div>
+          <p
+            style={{
+              color: "#2e2e2e",
+              fontWeight: "700",
+              fontFamily: "font-family: Palatino, Georgia, serif",
+            }}
+          >
+            {result.price}{" "}
+            <span
+              style={{
+                color: "#568c40",
+              }}
+            >
+              $ Per Month
+            </span>{" "}
           </p>
 
-          <p>
-            <span>price:</span> {result.price} jd
+          <p style={{ color: "#2e2e2e", fontWeight: "400" }}>
+            {result.Qualifications}{" "}
           </p>
 
-          <p>
-            <span>Qualifications:</span> {result.Qualifications}{" "}
-          </p>
-
-          <p>
-            <span>practicalExperiences: </span>
+          <p style={{ color: "#2e2e2e", fontWeight: "400" }}>
             {result.practicalExperiences}{" "}
           </p>
-          <p className="doctor-description">{result.description}</p>
-          {token ? (
-            <>
-              {role_id == 2 ? (
-                ""
-              ) : (
-                <button className="btn-1" onClick={() => {}}>
-                  <span
-                    onClick={() => {
-                      history.push("/payment");
-                    }}
-                  >
-                    Subscribe
-                  </span>
-                </button>
-              )}
-            </>
-          ) : (
-            ""
-          )}
+          <p style={{ color: "#2e2e2e", fontWeight: "400" }}>
+            {result.description}
+          </p>
         </div>
+        {token ? (
+          <>
+            {role_id == 2 ? (
+              ""
+            ) : (
+              <button
+                style={{
+                  marginBottom: "20%",
+                  marginLeft: "20%",
+                  marginLeft: "10%",
+                }}
+                className="btn-1"
+                onClick={() => {
+                  history.push("/payment");
+                }}
+              >
+                Subscribe
+              </button>
+            )}
+          </>
+        ) : (
+          ""
+        )}
       </div>
 
       {token ? (
