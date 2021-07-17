a0d4803ee360a6ca928f24ec7ee7b25f243252ef
Ghaidaa_tabikh <tabikh.ghaidaa@gmail.com>
2021-07-17 03:08:52 +0300

    deploy

--- db/schema.sql
+++ db/schema.sql
@@ -1,260 +1,260 @@
-DROP DATABASE MERAKI_Academy_Project_5;
-CREATE DATABASE MERAKI_Academy_Project_5;
+-- DROP DATABASE MERAKI_Academy_Project_5;
+-- CREATE DATABASE MERAKI_Academy_Project_5;
 
-USE MERAKI_Academy_Project_5;
-
-
-Create table roles (
- role_id INT AUTO_INCREMENT NOT NULL,
- role VARCHAR(255) NOT NULL,
- primary key (role_id)
-);
+-- USE MERAKI_Academy_Project_5;
 
 
-CREATE TABLE users(
- id INT AUTO_INCREMENT NOT NULL,
- firstName VARCHAR(255),
- lastName VARCHAR(255),
- age INT(3),
- email  VARCHAR(255),
- password VARCHAR(255),
- role_id INT,
- FOREIGN KEY (role_id) REFERENCES roles (role_id),
- is_deleted TINYINT DEFAULT 0,
- img TEXT,
- PRIMARY KEY (id)
-);
+-- Create table roles (
+--  role_id INT AUTO_INCREMENT NOT NULL,
+--  role VARCHAR(255) NOT NULL,
+--  primary key (role_id)
+-- );
 
 
-CREATE TABLE doctors (
- doctor_id INT AUTO_INCREMENT NOT NULL,
- firstName VARCHAR(255) ,
- lastName Varchar(255),
- age Varchar(255),
- description TEXT,
- email VARCHAR(255) NOT NULL,
- Qualifications VARCHAR (255) ,
- practicalExperiences VARCHAR(255) , 
- qualificationsFile VARCHAR(255),
- is_deleted TINYINT DEFAULT 0,
- primary key(doctor_id)
-);
+-- CREATE TABLE users(
+--  id INT AUTO_INCREMENT NOT NULL,
+--  firstName VARCHAR(255),
+--  lastName VARCHAR(255),
+--  age INT(3),
+--  email  VARCHAR(255),
+--  password VARCHAR(255),
+--  role_id INT,
+--  FOREIGN KEY (role_id) REFERENCES roles (role_id),
+--  is_deleted TINYINT DEFAULT 0,
+--  img TEXT,
+--  PRIMARY KEY (id)
+-- );
 
 
-CREATE TABLE doctorsDetails (
- id INT AUTO_INCREMENT NOT NULL,
- description TEXT,
- Qualifications TEXT ,
- practicalExperiences TEXT , 
- price INT,
- user_id INT,
- is_deleted TINYINT DEFAULT 0,
- FOREIGN KEY (user_id) REFERENCES users(id),
- primary key (id)
-);
+-- CREATE TABLE doctors (
+--  doctor_id INT AUTO_INCREMENT NOT NULL,
+--  firstName VARCHAR(255) ,
+--  lastName Varchar(255),
+--  age Varchar(255),
+--  description TEXT,
+--  email VARCHAR(255) NOT NULL,
+--  Qualifications VARCHAR (255) ,
+--  practicalExperiences VARCHAR(255) , 
+--  qualificationsFile VARCHAR(255),
+--  is_deleted TINYINT DEFAULT 0,
+--  primary key(doctor_id)
+-- );
 
 
-CREATE TABLE breakfast (
- breakfast_id INT AUTO_INCREMENT NOT NULL,
- name varchar(255),
-  date DATE ,
- calories varchar(255),
- carbohydrates_total_g varchar(255),
- cholesterol_mg varchar(255),
- fat_saturated_g varchar(255),
- fiber_g varchar(255),
- potassium_mg varchar(255),
- protein_g varchar(255),
- serving_size_g varchar(255),
- sodium_mg varchar(255),
- sugar_g varchar(255),
- user_id INT,
- FOREIGN KEY (user_id) REFERENCES users(id),
- primary key (breakfast_id)
+-- CREATE TABLE doctorsDetails (
+--  id INT AUTO_INCREMENT NOT NULL,
+--  description TEXT,
+--  Qualifications TEXT ,
+--  practicalExperiences TEXT , 
+--  price INT,
+--  user_id INT,
+--  is_deleted TINYINT DEFAULT 0,
+--  FOREIGN KEY (user_id) REFERENCES users(id),
+--  primary key (id)
+-- );
 
-);
 
-CREATE TABLE snack (
- snack_id INT AUTO_INCREMENT NOT NULL,
- name varchar(255),
-  date DATE ,
-  calories varchar(255),
- carbohydrates_total_g varchar(255),
- cholesterol_mg varchar(255),
- fat_saturated_g varchar(255),
- fiber_g varchar(255),
- potassium_mg varchar(255),
- protein_g varchar(255),
- serving_size_g varchar(255),
- sodium_mg varchar(255),
- sugar_g varchar(255),
- user_id INT,
- FOREIGN KEY (user_id) REFERENCES users(id),
- primary key (snack_id)
-);
+-- CREATE TABLE breakfast (
+--  breakfast_id INT AUTO_INCREMENT NOT NULL,
+--  name varchar(255),
+--   date DATE ,
+--  calories varchar(255),
+--  carbohydrates_total_g varchar(255),
+--  cholesterol_mg varchar(255),
+--  fat_saturated_g varchar(255),
+--  fiber_g varchar(255),
+--  potassium_mg varchar(255),
+--  protein_g varchar(255),
+--  serving_size_g varchar(255),
+--  sodium_mg varchar(255),
+--  sugar_g varchar(255),
+--  user_id INT,
+--  FOREIGN KEY (user_id) REFERENCES users(id),
+--  primary key (breakfast_id)
 
-CREATE TABLE lunch (
- lunch_id INT AUTO_INCREMENT NOT NULL,
- name varchar(255),
-  date DATE ,
-  calories varchar(255),
- carbohydrates_total_g varchar(255),
- cholesterol_mg varchar(255),
- fat_saturated_g varchar(255),
- fiber_g varchar(255),
- potassium_mg varchar(255),
- protein_g varchar(255),
- serving_size_g varchar(255),
- sodium_mg varchar(255),
- sugar_g varchar(255),
- user_id INT,
- FOREIGN KEY (user_id) REFERENCES users(id),
- primary key (lunch_id)
-);
+-- );
 
-CREATE TABLE dinner (
- dinner_id INT AUTO_INCREMENT NOT NULL,
- name varchar(255),
-  date DATE ,
-  calories varchar(255),
- carbohydrates_total_g varchar(255),
- cholesterol_mg varchar(255),
- fat_saturated_g varchar(255),
- fiber_g varchar(255),
- potassium_mg varchar(255),
- protein_g varchar(255),
- serving_size_g varchar(255),
- sodium_mg varchar(255),
- sugar_g varchar(255),
- user_id INT,
- FOREIGN KEY (user_id) REFERENCES users(id),
- primary key (dinner_id)
-);
+-- CREATE TABLE snack (
+--  snack_id INT AUTO_INCREMENT NOT NULL,
+--  name varchar(255),
+--   date DATE ,
+--   calories varchar(255),
+--  carbohydrates_total_g varchar(255),
+--  cholesterol_mg varchar(255),
+--  fat_saturated_g varchar(255),
+--  fiber_g varchar(255),
+--  potassium_mg varchar(255),
+--  protein_g varchar(255),
+--  serving_size_g varchar(255),
+--  sodium_mg varchar(255),
+--  sugar_g varchar(255),
+--  user_id INT,
+--  FOREIGN KEY (user_id) REFERENCES users(id),
+--  primary key (snack_id)
+-- );
 
-CREATE TABLE glassesOfWater (
- glassesOfWater_id INT AUTO_INCREMENT NOT NULL,
- name varchar(255),
-  date DATE ,
-  calories varchar(255),
- carbohydrates_total_g varchar(255),
- cholesterol_mg varchar(255),
- fat_saturated_g varchar(255),
- fiber_g varchar(255),
- potassium_mg varchar(255),
- protein_g varchar(255),
- serving_size_g varchar(255),
- sodium_mg varchar(255),
- sugar_g varchar(255),
- user_id INT,
- FOREIGN KEY (user_id) REFERENCES users(id),
- primary key (glassesOfWater_id)
-);
+-- CREATE TABLE lunch (
+--  lunch_id INT AUTO_INCREMENT NOT NULL,
+--  name varchar(255),
+--   date DATE ,
+--   calories varchar(255),
+--  carbohydrates_total_g varchar(255),
+--  cholesterol_mg varchar(255),
+--  fat_saturated_g varchar(255),
+--  fiber_g varchar(255),
+--  potassium_mg varchar(255),
+--  protein_g varchar(255),
+--  serving_size_g varchar(255),
+--  sodium_mg varchar(255),
+--  sugar_g varchar(255),
+--  user_id INT,
+--  FOREIGN KEY (user_id) REFERENCES users(id),
+--  primary key (lunch_id)
+-- );
 
-CREATE TABLE activeTime (
- activeTime_id INT AUTO_INCREMENT NOT NULL,
- name varchar(255),
-  date DATE ,
- calories varchar(255),
- carbohydrates_total_g varchar(255),
- cholesterol_mg varchar(255),
- fat_saturated_g varchar(255),
- fiber_g varchar(255),
- potassium_mg varchar(255),
- protein_g varchar(255),
- serving_size_g varchar(255),
- sodium_mg varchar(255),
- sugar_g varchar(255),
- user_id INT,
- FOREIGN KEY (user_id) REFERENCES users(id),
- primary key (activeTime_id)
-);
+-- CREATE TABLE dinner (
+--  dinner_id INT AUTO_INCREMENT NOT NULL,
+--  name varchar(255),
+--   date DATE ,
+--   calories varchar(255),
+--  carbohydrates_total_g varchar(255),
+--  cholesterol_mg varchar(255),
+--  fat_saturated_g varchar(255),
+--  fiber_g varchar(255),
+--  potassium_mg varchar(255),
+--  protein_g varchar(255),
+--  serving_size_g varchar(255),
+--  sodium_mg varchar(255),
+--  sugar_g varchar(255),
+--  user_id INT,
+--  FOREIGN KEY (user_id) REFERENCES users(id),
+--  primary key (dinner_id)
+-- );
 
-CREATE TABLE foodTraker (
- foodTraker_id INT AUTO_INCREMENT NOT NULL,
- breakfast_id INT,
- snack_id INT,
- lunch_id INT,
- dinner_id INT,
- glassesOfWater_id INT,
- activeTime_id INT,
- is_deleted TINYINT DEFAULT 0,
- user_id INT,
- FOREIGN KEY (user_id) REFERENCES users(id),
- FOREIGN KEY (breakfast_id) REFERENCES breakfast(breakfast_id),
- FOREIGN KEY (snack_id) REFERENCES snack(snack_id),
- FOREIGN KEY (lunch_id) REFERENCES lunch(lunch_id),
- FOREIGN KEY (dinner_id) REFERENCES dinner(dinner_id),
- FOREIGN KEY (glassesOfWater_id) REFERENCES glassesOfWater(glassesOfWater_id),
- FOREIGN KEY (activeTime_id) REFERENCES activeTime(activeTime_id),
- primary key (foodTraker_id)
-);
+-- CREATE TABLE glassesOfWater (
+--  glassesOfWater_id INT AUTO_INCREMENT NOT NULL,
+--  name varchar(255),
+--   date DATE ,
+--   calories varchar(255),
+--  carbohydrates_total_g varchar(255),
+--  cholesterol_mg varchar(255),
+--  fat_saturated_g varchar(255),
+--  fiber_g varchar(255),
+--  potassium_mg varchar(255),
+--  protein_g varchar(255),
+--  serving_size_g varchar(255),
+--  sodium_mg varchar(255),
+--  sugar_g varchar(255),
+--  user_id INT,
+--  FOREIGN KEY (user_id) REFERENCES users(id),
+--  primary key (glassesOfWater_id)
+-- );
 
-CREATE TABLE Success (
- Success_id INT AUTO_INCREMENT NOT NULL,
- description VARCHAR(255),
- user_id INT,
- is_deleted TINYINT DEFAULT 0,
- FOREIGN KEY (user_id) REFERENCES users(id),
- primary key (Success_id )
-);
+-- CREATE TABLE activeTime (
+--  activeTime_id INT AUTO_INCREMENT NOT NULL,
+--  name varchar(255),
+--   date DATE ,
+--  calories varchar(255),
+--  carbohydrates_total_g varchar(255),
+--  cholesterol_mg varchar(255),
+--  fat_saturated_g varchar(255),
+--  fiber_g varchar(255),
+--  potassium_mg varchar(255),
+--  protein_g varchar(255),
+--  serving_size_g varchar(255),
+--  sodium_mg varchar(255),
+--  sugar_g varchar(255),
+--  user_id INT,
+--  FOREIGN KEY (user_id) REFERENCES users(id),
+--  primary key (activeTime_id)
+-- );
 
-CREATE TABLE reviews(
- id INT AUTO_INCREMENT NOT NULL,
- comment VARCHAR(255),
- doctorsService_id INT,
- FOREIGN KEY ( doctorsService_id ) REFERENCES doctorsDetails(id),
- commenter_id INT,
- rating INT ,
- is_deleted TINYINT DEFAULT 0,
- FOREIGN KEY (commenter_id) REFERENCES users(id),
- PRIMARY KEY (id)
-);
+-- CREATE TABLE foodTraker (
+--  foodTraker_id INT AUTO_INCREMENT NOT NULL,
+--  breakfast_id INT,
+--  snack_id INT,
+--  lunch_id INT,
+--  dinner_id INT,
+--  glassesOfWater_id INT,
+--  activeTime_id INT,
+--  is_deleted TINYINT DEFAULT 0,
+--  user_id INT,
+--  FOREIGN KEY (user_id) REFERENCES users(id),
+--  FOREIGN KEY (breakfast_id) REFERENCES breakfast(breakfast_id),
+--  FOREIGN KEY (snack_id) REFERENCES snack(snack_id),
+--  FOREIGN KEY (lunch_id) REFERENCES lunch(lunch_id),
+--  FOREIGN KEY (dinner_id) REFERENCES dinner(dinner_id),
+--  FOREIGN KEY (glassesOfWater_id) REFERENCES glassesOfWater(glassesOfWater_id),
+--  FOREIGN KEY (activeTime_id) REFERENCES activeTime(activeTime_id),
+--  primary key (foodTraker_id)
+-- );
 
-CREATE TABLE purchased(
- id INT AUTO_INCREMENT NOT NULL,
- doctorsService_id INT,
- time Varchar(255),
- date   Varchar(255) ,
- user_id INT,
- is_deleted TINYINT DEFAULT 0,
- FOREIGN KEY ( doctorsService_id ) REFERENCES doctorsDetails(id),
- FOREIGN KEY ( user_id  ) REFERENCES users(id),
- PRIMARY KEY (id)
-);
+-- CREATE TABLE Success (
+--  Success_id INT AUTO_INCREMENT NOT NULL,
+--  description VARCHAR(255),
+--  user_id INT,
+--  is_deleted TINYINT DEFAULT 0,
+--  FOREIGN KEY (user_id) REFERENCES users(id),
+--  primary key (Success_id )
+-- );
 
-CREATE TABLE schedule(
-id INT AUTO_INCREMENT NOT NULL,
-doctor_id INT ,
-user_id INT,
-time Varchar(255),
-date DATE ,
-is_deleted TINYINT DEFAULT 0,
-FOREIGN KEY (user_id) REFERENCES users(id),
-FOREIGN KEY (doctor_id) REFERENCES doctorsDetails(id),
-PRIMARY KEY (id)
-);
+-- CREATE TABLE reviews(
+--  id INT AUTO_INCREMENT NOT NULL,
+--  comment VARCHAR(255),
+--  doctorsService_id INT,
+--  FOREIGN KEY ( doctorsService_id ) REFERENCES doctorsDetails(id),
+--  commenter_id INT,
+--  rating INT ,
+--  is_deleted TINYINT DEFAULT 0,
+--  FOREIGN KEY (commenter_id) REFERENCES users(id),
+--  PRIMARY KEY (id)
+-- );
 
-CREATE TABLE conversation (
-id INT AUTO_INCREMENT NOT NULL,
-sender_id INT,
-reciver_id INT,
-FOREIGN KEY (sender_id) REFERENCES users(id),
-FOREIGN KEY (reciver_id) REFERENCES users(id),
-PRIMARY KEY (id)
-);
+-- CREATE TABLE purchased(
+--  id INT AUTO_INCREMENT NOT NULL,
+--  doctorsService_id INT,
+--  time Varchar(255),
+--  date   Varchar(255) ,
+--  user_id INT,
+--  is_deleted TINYINT DEFAULT 0,
+--  FOREIGN KEY ( doctorsService_id ) REFERENCES doctorsDetails(id),
+--  FOREIGN KEY ( user_id  ) REFERENCES users(id),
+--  PRIMARY KEY (id)
+-- );
 
-CREATE TABLE messages (
-id INT AUTO_INCREMENT NOT NULL ,
-message VARCHAR(255),
-sender_id INT,
-conversation_id INT,
-createdAt date ,
-FOREIGN KEY (sender_id) REFERENCES users(id),
-FOREIGN KEY (conversation_id) REFERENCES conversation(id),
-PRIMARY KEY (id)
+-- CREATE TABLE schedule(
+-- id INT AUTO_INCREMENT NOT NULL,
+-- doctor_id INT ,
+-- user_id INT,
+-- time Varchar(255),
+-- date DATE ,
+-- is_deleted TINYINT DEFAULT 0,
+-- FOREIGN KEY (user_id) REFERENCES users(id),
+-- FOREIGN KEY (doctor_id) REFERENCES doctorsDetails(id),
+-- PRIMARY KEY (id)
+-- );
 
-);
+-- CREATE TABLE conversation (
+-- id INT AUTO_INCREMENT NOT NULL,
+-- sender_id INT,
+-- reciver_id INT,
+-- FOREIGN KEY (sender_id) REFERENCES users(id),
+-- FOREIGN KEY (reciver_id) REFERENCES users(id),
+-- PRIMARY KEY (id)
+-- );
 
-INSERT INTO roles (role) VALUES ("user");
-INSERT INTO roles (role) VALUES ("doctor");
-INSERT INTO roles (role) VALUES ("admin");
+-- CREATE TABLE messages (
+-- id INT AUTO_INCREMENT NOT NULL ,
+-- message VARCHAR(255),
+-- sender_id INT,
+-- conversation_id INT,
+-- createdAt date ,
+-- FOREIGN KEY (sender_id) REFERENCES users(id),
+-- FOREIGN KEY (conversation_id) REFERENCES conversation(id),
+-- PRIMARY KEY (id)
+
+-- );
+
+-- INSERT INTO roles (role) VALUES ("user");
+-- INSERT INTO roles (role) VALUES ("doctor");
+-- INSERT INTO roles (role) VALUES ("admin");
